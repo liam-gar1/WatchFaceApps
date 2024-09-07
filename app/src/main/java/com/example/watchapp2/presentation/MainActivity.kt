827a650c01488c8039f7fff0362dc95355f96666
liam-gar1 <gar.liam3@gmail.com>
2024-09-06 22:17:13 -0400

    Changed the color of the text to Green

--- app/src/main/java/com/example/watchapp2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/watchapp2/presentation/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,7 +62,8 @@ fun Greeting(greetingName: String) {
     Text(
         modifier = Modifier.fillMaxWidth(),
         textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
+        //color = MaterialTheme.colors.primary,
+        color = Color(0xFF00FF00),
         //text = stringResource(R.string.hello_world, greetingName),
         text = "Sleep\nDuration App"
     )
